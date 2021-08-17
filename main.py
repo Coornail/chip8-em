42a14e7edc6af71cd653909c2fd87b1e55242c47
Kornel Lugosi <coornail@gmail.com>
2021-08-17 10:10:15 +0200

    Implement IBM drawing functions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 
+
 class chip8:
     memory = [0] * 4096
     display = [0] * 64 * 32
@@ -33,34 +34,36 @@ class chip8:
         elif opcode == 0x00E0:
             self.display = [0] * 64 * 32
             self.paint()
-        elif hex_opcode[2] == 'a':  # aXXX
-            self.i = opcode & 0x0FFF
+        elif hex_opcode[2] == '1':
+            self.pc = opcode & 0x0FFF
         elif hex_opcode[2] == '6':  # 6XXX
             self.registers[(opcode & 0x0F00) >> 8] = opcode & 0x00FF
+        elif hex_opcode[2] == '7':
+            self.registers[(opcode & 0x0F00) >> 8] += opcode & 0x00FF
+        elif hex_opcode[2] == 'a':  # aXXX
+            self.i = opcode & 0x0FFF
         elif hex_opcode[2] == 'd':  # DXYN
             x = self.registers[(opcode & 0x0F00) >> 8]
             y = self.registers[(opcode & 0x00F0) >> 4]
             n = opcode & 0x000F
-            
+
             self.registers[0xF] = 0
             for y_line in range(0, n):
                 pixel = self.memory[self.i + y_line]
                 for x_line in range(0, 8):
                     if (pixel & (0x80 >> x_line)) != 0:
-                        if self.display[(x + x_line + (y + y_line * 64))] == 1:
+                        if self.display[x + x_line + ((y + y_line) * 64)] == 1:
                             self.registers[0xF] = 1
-                        self.display[(x + x_line + (y + y_line * 64))] ^= 1
+                        self.display[x + x_line + ((y + y_line) * 64)] ^= 1
 
             self.paint()
-
-            return
         else:
             print("Unknown opcode: " + self.asm())
 
         self.pc = (self.pc + 2 % 4096)
 
     def decode(self):
-        return self.memory[self.pc] << 8 | self.memory[self.pc + 1] 
+        return self.memory[self.pc] << 8 | self.memory[self.pc + 1]
 
     def __str__(self) -> str:
         return 'inst: {0} [pc: {1}]\nmemory: {2}'.format(self.asm(), self.pc, self.memory)
